50f961467c99d690380ae778c0de466755a155ce
Delvoid <delvoidltd@gmail.com>
2021-12-13 15:53:44 +0000

    verify page update

--- src/pages/Verify.js
+++ src/pages/Verify.js
@@ -36,7 +36,7 @@ const Verify = () => {
 
   if (loading) {
     return (
-      <div className="page">
+      <div className="p-4 lg:w-1/3 md:w-2/3 m-auto justify-center">
         <h2>Loading...</h2>
       </div>
     )
@@ -51,11 +51,16 @@ const Verify = () => {
   }
 
   return (
-    <div className="page">
-      <h2>Account Confirmed</h2>
-      <Link to="/login" className="btn">
-        Please login
-      </Link>
+    <div className="p-4 lg:w-1/3 md:w-2/3 m-auto justify-center">
+      <div className="py-6 px-8 mt-20  bg-blue-100 rounded-lg shadow-xl">
+        <h2>Account Confirmed</h2>
+        <Link
+          to="/login"
+          className="bg-green-400 px-4 py-2 rounded text-white hover:bg-green-500 "
+        >
+          Please login
+        </Link>
+      </div>
     </div>
   )
 }
